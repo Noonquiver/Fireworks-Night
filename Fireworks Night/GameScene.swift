494f619e93da955cc5eab5fe1d47bacc543f3208
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-26 11:22:20 -0500

    first commit

---
+++ Fireworks Night/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  Fireworks Night
+//
+//  Created by Camilo Hernández Guerrero on 26/07/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+}
